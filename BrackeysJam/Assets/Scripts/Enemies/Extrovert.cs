926f4520268414cd5cc2a4a619b45464772407c2
Jem Cope <thejemmeh@gmail.com>
2022-08-22 01:49:06 -0700

    working on enemy pathing

---
+++ BrackeysJam/Assets/Scripts/Enemies/Extrovert.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Extrovert : MonoBehaviour
+{
+    public Transform pathHolder;
+
+    private void OnDrawGizmos()
+    {
+        Vector3 startPosition = pathHolder.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach (Transform waypoint in pathHolder)
+        {
+            Gizmos.DrawSphere(waypoint.position, .3f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+    }
+}
