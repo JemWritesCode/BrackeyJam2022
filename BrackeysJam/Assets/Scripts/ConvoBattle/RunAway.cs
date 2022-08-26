53768316e3fee95a67ab2ff1e0c6018f5d012d71
Jem Cope <thejemmeh@gmail.com>
2022-08-25 17:40:14 -0700

    Conversation Battle now pops up and unloads if you hit run away. isInBattle now works with that too.

--- BrackeysJam/Assets/Scripts/ConvoBattle/RunAway.cs
+++ BrackeysJam/Assets/Scripts/ConvoBattle/RunAway.cs
@@ -5,22 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class RunAway : MonoBehaviour
 {
+    SceneManagement sceneManagement;
+
+    public void RunAwayEndBattle()
+    {
+        GameObject sceneManager = GameObject.Find("SceneManager");
+        sceneManagement = sceneManager.GetComponent<SceneManagement>();
+        sceneManagement.isInBattle = false;
+        Debug.Log("Run Away Clicked");
+        SceneManager.UnloadSceneAsync("2-ConversationBattle");
+    }
+
+    //SceneManagement sceneManagement;
     //public void RunAwayEndBattle()
     //{
-    //    //GameObject sceneManager = GameObject.Find("SceneManager");
-    //    //sceneManagement = sceneManager.GetComponent<SceneManagement>();
-    //    //sceneManagement.isInBattle = false;
-    //    Debug.Log("Run Away Clicked");
-    //    SceneManager.UnloadSceneAsync("2-ConversationBattle");
+    //    sceneManagement.isInBattle = false;
+    //    Debug.Log("isInBattle: " + sceneManagement.isInBattle);
+    //    SceneManager.LoadScene("1-MainGame");
+    //    Debug.Log("Loading 1-MainGame...");
+    //    // isInBattle = false;
     //}
-
-    ////SceneManagement sceneManagement;
-    ////public void RunAwayEndBattle()
-    ////{
-    ////    sceneManagement.isInBattle = false;
-    ////    Debug.Log("isInBattle: " + sceneManagement.isInBattle);
-    ////    SceneManager.LoadScene("1-MainGame");
-    ////    Debug.Log("Loading 1-MainGame...");
-    ////    // isInBattle = false;
-    ////}
 }
