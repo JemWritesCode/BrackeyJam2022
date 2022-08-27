b6230a0a154375fa23fdaee5c947ee91620d7c4f
redseiko <redseiko@users.noreply.github.com>
2022-08-26 18:24:42 -0700

    Expand upon the ConversationBattle and LaneController.

--- BrackeysJam/Assets/Scripts/ConvoBattle/ConversationBattleManager.cs
+++ BrackeysJam/Assets/Scripts/ConvoBattle/ConversationBattleManager.cs
@@ -1,5 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEditor;
+
 using UnityEngine;
 
 public class ConversationBattleManager : MonoBehaviour {
+  [field: SerializeField]
+  public List<EmojiBattleLaneController> EmojiBattleLaneControllers { get; private set; } = new();
 
+}
+
+[CustomEditor(typeof(ConversationBattleManager))]
+[CanEditMultipleObjects]
+public class ConversationBattleManagerEditor : Editor {
+  private List<EmojiBattleLaneController> _controllers;
+
+  void Awake() {
+    _controllers = Selection.activeGameObject.GetComponent<ConversationBattleManager>().EmojiBattleLaneControllers;
+  }
+
+  public override void OnInspectorGUI() {
+    base.OnInspectorGUI();
+
+    foreach (EmojiBattleLaneController controller in _controllers) {
+      if (EditorGUILayout.BeginFoldoutHeaderGroup(true, controller.name)) {
+        if (GUILayout.Button("GenerateEmoji")) {
+          controller.GenerateEmoji();
+        }
+      }
+
+      EditorGUILayout.EndFoldoutHeaderGroup();
+    }
+  }
 }
--- BrackeysJam/Assets/Scripts/ConvoBattle/EmojiBattleLaneController.cs
+++ BrackeysJam/Assets/Scripts/ConvoBattle/EmojiBattleLaneController.cs
@@ -1,18 +1,30 @@
 using DG.Tweening;
 
+using UnityEditor;
+
 using UnityEngine;
 
 public class EmojiBattleLaneController : MonoBehaviour {
-  [field: SerializeField]
+  [field: SerializeField, Header("Key")]
   public KeyCode LaneKeyboardKey { get; private set; } = KeyCode.None;
 
-  [field: SerializeField, Header("LaneKey")]
+  [field: SerializeField]
   public GameObject KeyIcon { get; private set; }
 
   [field: SerializeField]
   public TMPro.TMP_Text KeyIconLabel { get; private set; }
 
+  [field: SerializeField, Header("Emoji")]
+  public TMPro.TMP_Text EmojiIcon { get; private set; }
+
+  [field: SerializeField]
+  public Ease EmojiChildMovementEase { get; private set; } = Ease.Linear;
+
+  [field: SerializeField, Header("HitArea")]
+  public GameObject HitArea { get; private set; }
+
   private Tweener _laneKeyDownTweener;
+  private Sequence _generateEmojiIconSequence;
 
   void Awake() {
     _laneKeyDownTweener =
@@ -20,11 +32,47 @@ public class EmojiBattleLaneController : MonoBehaviour {
             .DOPunchScale(new Vector3(1.05f, 1.05f, 1.05f), 0.25f, 10, 0.5f)
             .SetAutoKill(false)
             .Pause();
+
+    _generateEmojiIconSequence =
+        DOTween.Sequence()
+            .Insert(0f, EmojiIcon.transform.DOPunchScale(new Vector3(1.05f, 1.05f, 1.05f), 0.5f, 5, 0.5f))
+            .Insert(0f, EmojiIcon.DOFade(1f, 0.25f).SetLoops(2, LoopType.Yoyo))
+            .OnComplete(() => GenerateEmojiChild())
+            .SetAutoKill(false)
+            .Pause();
   }
 
   void Update() {
     if (Input.GetKeyDown(LaneKeyboardKey)) {
       _laneKeyDownTweener.Restart();
+    }
+  }
+
+  public void GenerateEmoji() {
+    _generateEmojiIconSequence.Restart();
+  }
+
+  public void GenerateEmojiChild() {
+    TMPro.TMP_Text child = Instantiate(EmojiIcon, EmojiIcon.transform.parent);
+    child.alpha = 1f;
+    child.transform.DOScale(2f, 2f);
+    child.transform.DOShakeRotation(2f, new Vector3(0f, 0f, 90f)).SetLoops(-1, LoopType.Yoyo);
+    child.transform
+        .DOLocalMove(HitArea.transform.localPosition, 15f)
+        .SetEase(EmojiChildMovementEase)
+        .OnComplete(() => Destroy(child));
+  }
+}
+
+[CustomEditor(typeof(EmojiBattleLaneController))]
+public class EmojiBattleLaneControllerEditor : Editor {
+  public override void OnInspectorGUI() {
+    base.OnInspectorGUI();
+
+    EditorGUILayout.Space(20f);
+
+    if (GUILayout.Button("Generate")) {
+      Selection.activeGameObject.GetComponent<EmojiBattleLaneController>().GenerateEmoji();
     }
   }
 }
